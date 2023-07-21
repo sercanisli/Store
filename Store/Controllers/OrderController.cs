c3e62e89188aaa7d22b98d316f5a182242161ab0
Sercan ISLI <sercanislii@gmail.com>
2023-07-21 17:38:06 +0300

    Order Process are done

---
+++ Store/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace Store.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly Cart _cart;
+
+        public OrderController(IServiceManager serviceManager, Cart cart)
+        {
+            _serviceManager = serviceManager;
+            _cart = cart;
+        }
+
+        [HttpGet]
+       public ViewResult Checkout() => View(new Order());
+
+       [HttpPost]
+        [ValidateAntiForgeryToken]
+       public IActionResult Checkout([FromForm]Order order)
+       {
+           if (_cart.Lines.Count() == 0)
+           {
+                ModelState.AddModelError("","Sorry, your cart is empty");
+           }
+
+           if (ModelState.IsValid)
+           {
+               order.Lines = _cart.Lines.ToArray();
+               _serviceManager.OrderService.SaveOrder(order); 
+               _cart.Clear(); 
+               return RedirectToPage("/Complete", new { OrderId = order.Id });
+           }
+           return View();
+       }
+    }
+}
